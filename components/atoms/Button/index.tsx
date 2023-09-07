3f67f88b23b670a86b230786dc04d0386f8833ce
Muhammad Anies Wahdie <manies5396@gmail.com>
2023-09-07 13:45:04 +0700

    fix: isFocus button

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -3,8 +3,8 @@
 import React, {ButtonHTMLAttributes, useMemo, useState} from "react";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-	variant?: "primary" | "primary-outline";
-	size?: "sm" | "md";
+	variant?: "primary" | "primary-outline" | "secondary" | "secondary-outline";
+	size?: "sm" | "md" | "lg";
 };
 
 // eslint-disable-next-line react/display-name
@@ -15,19 +15,23 @@ const Button = React.forwardRef<HTMLButtonElement, Props>((props, ref) => {
 		variant = "primary",
 		onMouseOver,
 		onMouseLeave,
+		onClick,
 		style,
 		className,
 		...attrs
 	} = props;
 
 	const [isHover, setIsHover] = useState(false);
+	const [isFocus, setIsFocus] = useState(false);
 
 	const buttonSize = useMemo(() => {
 		switch (size) {
 			case "sm":
 				return "h-8";
+			case "md":
+				return "h-10";
 			default:
-				return "h-14";
+				return "h-12";
 		}
 	}, [size]);
 
@@ -45,31 +49,72 @@ const Button = React.forwardRef<HTMLButtonElement, Props>((props, ref) => {
 				}
 			case "primary-outline":
 				return "transparent";
+			case "secondary":
+				if (disabled) {
+					return "#D3D4D4";
+				} else {
+					if (isHover) {
+						return "#FC9641";
+					} else {
+						return "#F87304";
+					}
+				}
+			case "secondary-outline":
+				return "transparent";
 		}
 	}, [variant, disabled, isHover]);
 
 	const borderColor = useMemo(() => {
+		if (disabled) {
+			return "#777777";
+		}
 		switch (variant) {
 			case "primary":
+				if (isFocus) {
+					return "#9ACEF7";
+				}
 				return "#1078CA";
 			case "primary-outline":
-				if (disabled) {
-					return "#777777";
+				if (isHover) {
+					return "#1078CA";
 				} else {
-					if (isHover) {
-						return "#1078CA";
-					} else {
-						return "#1078CA";
-					}
+					return "#1078CA";
+				}
+			case "secondary":
+				if (isFocus) {
+					return "#D16104";
+				}
+				return "#F87304";
+			case "secondary-outline":
+				if (isHover) {
+					return "#F87304";
+				} else {
+					return "#F87304";
 				}
 		}
-	}, [variant, disabled, isHover]);
+	}, [variant, disabled, isHover, isFocus]);
+
+	const borderWidth = useMemo(() => {
+		if (isFocus) {
+			return 2;
+		}
+		switch (variant) {
+			case "primary-outline":
+			case "secondary-outline":
+				return 1;
+			default:
+				return 0;
+		}
+	}, [variant, isFocus]);
 
 	const textSize = useMemo(() => {
 		if (size === "sm") {
+			return "text-xs";
+		} else if (size === "md") {
 			return "text-sm";
+		} else {
+			return "text-base";
 		}
-		return "text-base";
 	}, [size]);
 
 	const textColor = useMemo(() => {
@@ -90,8 +135,35 @@ const Button = React.forwardRef<HTMLButtonElement, Props>((props, ref) => {
 						return "#1078CA";
 					}
 				}
+			case "secondary":
+				if (disabled) {
+					return "#777777";
+				} else {
+					return "#FFFFFF";
+				}
+			case "secondary-outline":
+				if (disabled) {
+					return "#777777";
+				} else {
+					if (isHover) {
+						return "#F87304";
+					} else {
+						return "#F87304";
+					}
+				}
 		}
 	}, [variant, disabled, isHover]);
+
+	const padding = useMemo(() => {
+		switch (size) {
+			case "sm":
+				return "p-2";
+			case "md":
+				return "py-2 px-3";
+			default:
+				return "p-3";
+		}
+	}, [size]);
 
 	return (
 		<button
@@ -110,12 +182,29 @@ const Button = React.forwardRef<HTMLButtonElement, Props>((props, ref) => {
 
 				onMouseLeave(e);
 			}}
-			className={"rounded font-semibold px-6"
-				.concat(textSize ? " " + textSize : "")
+			onClick={(e) => {
+				setIsFocus(!isFocus);
+
+				if (!onClick) return;
+
+				onClick(e);
+			}}
+			className={"rounded-lg font-semibold "
+				.concat(textSize ? " " + textSize + " " + padding : "")
 				.concat(buttonSize ? " " + buttonSize : "")
-				.concat(variant === "primary-outline" ? " border" : "")
+				.concat(
+					variant === "primary-outline" || variant === "secondary-outline"
+						? " border"
+						: "",
+				)
 				.concat(className ? " " + className : "")}
-			style={{borderColor, backgroundColor, color: textColor, ...style}}
+			style={{
+				borderColor,
+				backgroundColor,
+				color: textColor,
+				borderWidth,
+				...style,
+			}}
 			{...attrs}
 			ref={ref}
 		/>
