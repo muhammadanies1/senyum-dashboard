765ea3866f89fbc9c26d1d007acc364eb7436dc9
pradiptaagus <igedeaguspradipta@gmail.com>
2023-09-08 14:23:12 +0700

    feat: button css class

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -1,214 +1,34 @@
-"use client";
+import "./button.module.css";
 
-import React, {ButtonHTMLAttributes, useMemo, useState} from "react";
+import React, {ButtonHTMLAttributes} from "react";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-	variant?: "primary" | "primary-outline" | "secondary" | "secondary-outline";
+	variant?: "primary" | "secondary";
+	bordered?: boolean;
 	size?: "sm" | "md" | "lg";
 };
 
-// eslint-disable-next-line react/display-name
 const Button = React.forwardRef<HTMLButtonElement, Props>((props, ref) => {
 	const {
-		disabled,
 		size = "md",
 		variant = "primary",
-		onMouseOver,
-		onMouseLeave,
-		onClick,
-		style,
+		bordered,
 		className,
 		...attrs
 	} = props;
 
-	const [isHover, setIsHover] = useState(false);
-	const [isFocus, setIsFocus] = useState(false);
-
-	const buttonSize = useMemo(() => {
-		switch (size) {
-			case "sm":
-				return "h-8";
-			case "md":
-				return "h-10";
-			default:
-				return "h-12";
-		}
-	}, [size]);
-
-	const backgroundColor = useMemo(() => {
-		switch (variant) {
-			case "primary":
-				if (disabled) {
-					return "#D3D4D4";
-				} else {
-					if (isHover) {
-						return "#138CEC";
-					} else {
-						return "#1078CA";
-					}
-				}
-			case "primary-outline":
-				return "transparent";
-			case "secondary":
-				if (disabled) {
-					return "#D3D4D4";
-				} else {
-					if (isHover) {
-						return "#FC9641";
-					} else {
-						return "#F87304";
-					}
-				}
-			case "secondary-outline":
-				return "transparent";
-		}
-	}, [variant, disabled, isHover]);
-
-	const borderColor = useMemo(() => {
-		if (disabled) {
-			return "#777777";
-		}
-		switch (variant) {
-			case "primary":
-				if (isFocus) {
-					return "#9ACEF7";
-				}
-				return "#1078CA";
-			case "primary-outline":
-				if (isHover) {
-					return "#1078CA";
-				} else {
-					return "#1078CA";
-				}
-			case "secondary":
-				if (isFocus) {
-					return "#D16104";
-				}
-				return "#F87304";
-			case "secondary-outline":
-				if (isHover) {
-					return "#F87304";
-				} else {
-					return "#F87304";
-				}
-		}
-	}, [variant, disabled, isHover, isFocus]);
-
-	const borderWidth = useMemo(() => {
-		if (isFocus) {
-			return 2;
-		}
-		switch (variant) {
-			case "primary-outline":
-			case "secondary-outline":
-				return 1;
-			default:
-				return 0;
-		}
-	}, [variant, isFocus]);
-
-	const textSize = useMemo(() => {
-		if (size === "sm") {
-			return "text-xs";
-		} else if (size === "md") {
-			return "text-sm";
-		} else {
-			return "text-base";
-		}
-	}, [size]);
-
-	const textColor = useMemo(() => {
-		switch (variant) {
-			case "primary":
-				if (disabled) {
-					return "#777777";
-				} else {
-					return "#FFFFFF";
-				}
-			case "primary-outline":
-				if (disabled) {
-					return "#777777";
-				} else {
-					if (isHover) {
-						return "#1078CA";
-					} else {
-						return "#1078CA";
-					}
-				}
-			case "secondary":
-				if (disabled) {
-					return "#777777";
-				} else {
-					return "#FFFFFF";
-				}
-			case "secondary-outline":
-				if (disabled) {
-					return "#777777";
-				} else {
-					if (isHover) {
-						return "#F87304";
-					} else {
-						return "#F87304";
-					}
-				}
-		}
-	}, [variant, disabled, isHover]);
-
-	const padding = useMemo(() => {
-		switch (size) {
-			case "sm":
-				return "px-2";
-			case "md":
-				return "px-3";
-			default:
-				return "px-4";
-		}
-	}, [size]);
-
 	return (
 		<button
-			disabled={disabled}
-			onMouseOver={(e) => {
-				setIsHover(true);
-
-				if (!onMouseOver) return;
-
-				onMouseOver(e);
-			}}
-			onMouseLeave={(e) => {
-				setIsHover(false);
-
-				if (!onMouseLeave) return;
-
-				onMouseLeave(e);
-			}}
-			onClick={(e) => {
-				setIsFocus(!isFocus);
-
-				if (!onClick) return;
-
-				onClick(e);
-			}}
-			className={"rounded-lg font-semibold "
-				.concat(textSize ? " " + textSize + " " + padding : "")
-				.concat(buttonSize ? " " + buttonSize : "")
-				.concat(
-					variant === "primary-outline" || variant === "secondary-outline"
-						? " border"
-						: "",
-				)
-				.concat(className ? " " + className : "")}
-			style={{
-				borderColor,
-				backgroundColor,
-				color: textColor,
-				borderWidth,
-				...style,
-			}}
+			className={"btn"
+				.concat(variant ? ` ${variant}` : "")
+				.concat(size ? ` ${size}` : "")
+				.concat(bordered ? ` bordered` : "")}
 			{...attrs}
 			ref={ref}
 		/>
 	);
 });
+
+Button.displayName = "Button";
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,27 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				light: {
+					30: "#D3D4D4",
+					80: "#777777",
+				},
+				primary: {
+					10: "#DDEFFC",
+					30: "#9ACEF7",
+					40: "#78BDF4",
+					70: "#138CEC",
+					80: "#1078CA",
+					100: "#0C5A98",
+				},
+				secondary: {
+					10: "#FEEDDF",
+					80: "#F87304",
+					100: "#D16104",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
