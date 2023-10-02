60e980ac5d69e4bfa84b96bbb08826b93ca36e21
Pradipta Agus <igedeaguspradipta@gmail.com>
2023-10-02 02:02:02 +0000

    Merged in fix/import (pull request #60)
    fix: invalid import path
    
    * fix: invalid import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 import Filter from "../components/atoms/Filter";
-import ModalAddUser from "./modal-add-user";
 
 export default function Page() {
 	return (
 		<React.Fragment>
 			<h1>Hello, Next.js!</h1>
-			<ModalAddUser />
-			{/* <Sidebar /> */}
-			<Filter className="bg-primary-10">Filter</Filter>
 		</React.Fragment>
 	);
 }
